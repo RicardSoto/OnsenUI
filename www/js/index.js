a1d6a1823a5478e327194b7aaaade830eabae560
RicardSoto <R.soto1998@gmail.com>
2020-10-24 08:25:34 -0500

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,40 +3,38 @@ function takePicture(){
         destinationType: Camera.DestinationType.DATA_URL });
 }
 function successCallback(imageData) {
-    document.getElementById("img").remove();
-    var x = document.createElement("img");
-    x.setAttribute("width","280");
-    x.setAttribute("height","180");
-    x.setAttribute("id","img");
-    x.setAttribute("src","data:image/jpeg;base64, " + imageData);
-    document.getElementById('pic').appendChild(x);
+  document.getElementById("img").remove();
+  var x = document.createElement("img");
+  x.setAttribute("width","280");
+  x.setAttribute("height","180");
+  x.setAttribute("id","img");
+  x.setAttribute("src","data:image/jpeg;base64, " + imageData);
+  document.getElementById('pic').appendChild(x);
 }
-
 function takeVideo() {
-    navigator.device.capture.captureVideo(captureSuccess, FailCallBack, { limit: 1 });
+  navigator.device.capture.captureVideo(captureSuccess, FailCallBack, { limit: 1 });
 }
 function captureSuccess(mediaFiles) {
-    var i, path, len;
-    for (i = 0, len = mediaFiles.length; i < len; i += 1) {
-      path = mediaFiles[i].fullPath;
-      document.getElementById("VIDEO").remove();
-      var x = document.createElement("VIDEO");
+  var i, path, len;
+  for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+    path = mediaFiles[i].fullPath;
+    document.getElementById("VIDEO").remove();
+    var x = document.createElement("VIDEO");
 
-      if (x.canPlayType("video/mp4")) {
-        x.setAttribute("src", path);
-      } else {
-        x.setAttribute("src", "https://video.twimg.com/ext_tw_video/1244003140183060485/pu/vid/1280x720/nyR7xJnXM6sBC_6L.mp4");
-      }
-
-      x.setAttribute("width", "280");
-      x.setAttribute("height", "180");
-      x.setAttribute("controls", "controls");
-      x.setAttribute("autoplay","autoplay");
-      x.setAttribute("id","VIDEO");
-      document.getElementById('wea').appendChild(x);
+    if (x.canPlayType("video/mp4")) {
+      x.setAttribute("src", path);
+    } else {
+      x.setAttribute("src", "https://video.twimg.com/ext_tw_video/1244003140183060485/pu/vid/1280x720/nyR7xJnXM6sBC_6L.mp4");
     }
-};
 
+    x.setAttribute("width", "280");
+    x.setAttribute("height", "180");
+    x.setAttribute("controls", "controls");
+    x.setAttribute("autoplay","autoplay");
+    x.setAttribute("id","VIDEO");
+    document.getElementById('wea').appendChild(x);
+  }
+}
 function FailCallBack (message){
-    console.log("Error: ",message);
+  console.log("Error: ",message);
 }
